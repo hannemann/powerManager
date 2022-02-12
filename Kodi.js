5f7f0a4cdfb80dbefa49a01b4e38797a1698f2c1
hannemann <hannemann666@gmx.net>
2022-02-12 22:54:12 +0100

    init screensaver polling

--- Kodi.js
+++ Kodi.js
@@ -1,51 +1,61 @@
-var http = require('http');
+var http = require("http");
 
-var Kodi = function () {};
+var Kodi = function () {
+  this.requestId = 1;
+  this.callbacks = {};
+  this.requests = {};
+};
 
 Kodi.prototype.isActiveData = {
-  "jsonrpc": "2.0",
-  "id": 1,
-  "method": "XBMC.GetInfoBooleans",
-  "params": {
-    "booleans": [
-      "System.ScreenSaverActive"
-    ]
-  }
+  jsonrpc: "2.0",
+  id: 1,
+  method: "XBMC.GetInfoBooleans",
+  params: {
+    booleans: ["System.ScreenSaverActive"],
+  },
 };
 
 Kodi.prototype.getActivePlayerData = {
-  "jsonrpc": "2.0",
-  "id": 1,
-  "method": "Player.GetActivePlayers"
+  jsonrpc: "2.0",
+  id: 1,
+  method: "Player.GetActivePlayers",
 };
 
-Kodi.prototype.stopData =  {
-  "jsonrpc": "2.0",
-  "id": 1,
-  "method": "Player.Stop",
-  "params": {
-    "playerid": 1
-  }
+Kodi.prototype.stopData = {
+  jsonrpc: "2.0",
+  id: 1,
+  method: "Player.Stop",
+  params: {
+    playerid: 1,
+  },
+};
+
+Kodi.prototype.activateScreensaverData = {
+  jsonrpc: "2.0",
+  method: "GUI.ActivateWindow",
+  id: 1,
+  params: {
+    window: "screensaver",
+  },
 };
 
 Kodi.prototype.options = {
-  "hostname" : process.env.ADDRESS,
-  "port" : process.env.KODI_PORT,
-  "path" : "/jsonrpc",
-  "method" : "POST",
-  "headers" : {
-    "Content-Type": "application/json"
-  }
+  hostname: process.env.ADDRESS,
+  port: process.env.KODI_PORT,
+  path: "/jsonrpc",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
 };
 
 Kodi.prototype.init = function () {
   this.responseHandler = this.handleResponse.bind(this);
   this.errorHandler = this.handleError.bind(this);
-  console.log('Kodi module initialized');
+  console.log("Kodi module initialized");
 };
 
 Kodi.prototype.getClient = function () {
-
   if ("undefined" !== typeof this.client) {
     this.removeObserver();
   }
@@ -54,28 +64,24 @@ Kodi.prototype.getClient = function () {
 };
 
 Kodi.prototype.addObserver = function () {
-
-  this.request.on('error', this.handleError.bind(this));
+  this.request.on("error", this.handleError.bind(this));
 };
 
 Kodi.prototype.removeObserver = function () {
-
-    this.request.removeListener('data', this.responseHandler);
-    this.request.removeListener('error', this.errorHandler);
+  this.request.removeListener("data", this.responseHandler);
+  this.request.removeListener("error", this.errorHandler);
 };
 
 Kodi.prototype.handleError = function (e) {
-  console.log('Kodi: problem with request: ' + e.message);
+  console.log("Kodi: problem with request: " + e.message);
 };
 
 Kodi.prototype.handleRequest = function (result) {
-
-  result.setEncoding('utf8');
-  result.on('data', this.handleResponse.bind(this));
+  result.setEncoding("utf8");
+  result.on("data", this.handleResponse.bind(this));
 };
 
 Kodi.prototype.handleResponse = function (response) {
-
   var status;
 
   response = JSON.parse(response);
@@ -83,66 +89,109 @@ Kodi.prototype.handleResponse = function (response) {
   if (response.error) {
     console.log(response.error);
   } else {
-
-    if (response.result && response.result['System.ScreenSaverActive']) {
-      response = !(response.result['System.ScreenSaverActive']);
+    const id = response.id.toString();
+    switch (this.requests[id]) {
+      case "isActive":
+        response = !response.result["System.ScreenSaverActive"];
+        break;
+      case "hasActivePlayer":
+        response = response.length === 0 ? false : response;
+        break;
+      case "stop":
+        break;
+      case "activateScreenSaver":
+        break;
     }
-    if ("function" === typeof this.callback) {
-      this.callback(response);
-      this.callback = null;
+    if ("function" === typeof this.callbacks[id]) {
+      this.callbacks[id](response);
+      delete this.callbacks[id];
     }
   }
 };
 
 Kodi.prototype.isActive = function (callback) {
-
   this.getClient();
 
+  const requestData = this.isActiveData;
+  requestData.id = this.requestId;
   if ("function" === typeof callback) {
-    this.callback = callback;
+    this.callbacks[this.requestId] = callback;
   }
-
-  console.log(JSON.stringify(this.isActiveData));
+  this.requests[this.requestId] = "isActive";
+  this.requestId++;
 
   this.request.write(JSON.stringify(this.isActiveData));
   this.request.end();
 };
 
 Kodi.prototype.hasActivePlayer = function (callback) {
-
   this.getClient();
 
+  const requestData = this.getActivePlayerData;
+  requestData.id = this.requestId;
   if ("function" === typeof callback) {
-    this.callback = callback;
+    this.callbacks[this.requestId] = callback;
   }
+  this.requests[this.requestId] = "hasActivePlayer";
+  this.requestId++;
 
-  this.request.write(JSON.stringify(this.getActivePlayerData));
+  this.request.write(JSON.stringify(requestData));
   this.request.end();
 };
 
 Kodi.prototype.stop = function (id, callback) {
-
   this.getClient();
 
+  const requestData = this.stopData;
+  requestData.id = this.requestId;
   if ("function" === typeof callback) {
-    this.callback = callback;
+    this.callbacks[this.requestId] = callback;
   }
-  this.stopData.params.playerid = id;
-  this.request.write(JSON.stringify(this.stopData));
+  this.requests[this.requestId] = "stop";
+  this.requestId++;
+
+  requestData.params.playerid = id;
+  this.request.write(JSON.stringify(requestData));
   this.request.end();
 };
 
 Kodi.prototype.stopActivePlayers = function (callback) {
+  this.hasActivePlayer(
+    function (e) {
+      if (e.result && e.result.length > 0) {
+        setTimeout(
+          function () {
+            e.result.forEach(
+              function (player, k) {
+                const cb = k === e.result.length - 1 ? callback : undefined;
+                this.stop(player.playerid, cb);
+              }.bind(this)
+            );
+          }.bind(this),
+          1000
+        );
+      }
+    }.bind(this)
+  );
+};
 
-  this.hasActivePlayer(function (e) {
-    if (e.result && e.result.length > 0) {
-      setTimeout(function () {
-        e.result.forEach(function (player) {
-          this.stop(player.playerid);
-        }.bind(this));
-      }.bind(this), 1000);
-    }
-  }.bind(this));
+/**
+ * seems not to work?
+ * @param {Function} callback
+ */
+Kodi.prototype.activateScreenSaver = function (callback) {
+  this.getClient();
+
+  const requestData = this.activateScreensaverData;
+  requestData.id = this.requestId;
+  if ("function" === typeof callback) {
+    this.callbacks[this.requestId] = callback;
+  }
+  this.requests[this.requestId] = "activateScreenSaver";
+  this.requestId++;
+
+  this.request.write(JSON.stringify(requestData));
+  this.request.end();
 };
 
 module.exports.Kodi = Kodi;
--- Server.js
+++ Server.js
@@ -1,19 +1,19 @@
-var http = require('http'),
-    Gembird = require('./Gembird.js'),
-    Squeeze = require('./Squeeze.js'),
-    Kodi = require('./Kodi.js'),
-    Syslogger = require('ain2'),
-    server;
+var http = require("http"),
+  Gembird = require("./Gembird.js"),
+  Squeeze = require("./Squeeze.js"),
+  Kodi = require("./Kodi.js"),
+  Syslogger = require("ain2"),
+  server;
 
 Server = function () {};
 
 Server.prototype.init = function () {
-  console.log('===== Initializing Powermanager =====');
-  console.log('Address: %s', process.env.ADDRESS);
-  console.log('Port: %s', process.env.PORT);
+  console.log("===== Initializing Powermanager =====");
+  console.log("Address: %s", process.env.ADDRESS);
+  console.log("Port: %s", process.env.PORT);
   this.server = http.createServer();
   this.server.listen(parseInt(process.env.PORT, 10), process.env.ADDRESS);
-  this.server.on('request', this.handleRequest.bind(this));
+  this.server.on("request", this.handleRequest.bind(this));
   this.gembird = new Gembird.Gembird();
   this.squeeze = new Squeeze.Squeeze();
   this.squeeze.init();
@@ -21,55 +21,102 @@ Server.prototype.init = function () {
   this.kodi.init();
   this.plugSpeaker = 3;
   this.plugMonitor = 4;
-  console.log('Powermanager initialized');
+  this.pollKodiScreensaver();
+  console.log("Powermanager initialized");
+};
+
+Server.prototype.pollKodiScreensaver = function () {
+  setInterval(
+    function () {
+      this.kodi.isActive(
+        function (active) {
+          if ("undefined" === typeof this.kodiActive) {
+            this.kodiActive = active;
+          } else {
+            console.log(
+              "Kodi screensaver  is " + (active ? "not active" : "active")
+            );
+            if (active !== this.kodiActive) {
+              console.log("Kodi screensaver state changed");
+              this.kodiActive = active;
+              if (active) {
+                this.startWatching();
+              } else {
+                this.stopWatching();
+              }
+            }
+          }
+        }.bind(this)
+      );
+    }.bind(this),
+    5000
+  );
 };
 
 Server.prototype.handleRequest = function (request, response) {
-
-  response.writeHead(200, {"Content-Type": "text/html"});
-  if (0 === request.url.indexOf('/startMusic')) {
-    console.log('Start Music');
-    console.log('activating speaker socket');
-    this.gembird.switch(this.plugSpeaker, true);
-    console.log('deactivating monitor socket');
-    this.gembird.switch(this.plugMonitor, false);
-    this.kodi.stopActivePlayers();
+  response.writeHead(200, { "Content-Type": "text/html" });
+  if (0 === request.url.indexOf("/startMusic")) {
+    this.startMusic();
   }
 
-  if (0 === request.url.indexOf('/stopMusic')) {
-    console.log('Stop Music');
-    this.kodi.isActive(function (state) {
-      console.log('Kodi screensaver  is ' + (state ? 'not active' : 'active'));
-      if (!state) {
-        console.log('deactivating speaker socket');
-        this.gembird.switch(this.plugSpeaker, false);
-      }
-    }.bind(this));
+  if (0 === request.url.indexOf("/stopMusic")) {
+    this.stopMusic();
   }
 
-  if (0 === request.url.indexOf('/startWatching')) {
-    console.log('Kodi screensaver deactivated');
-    console.log('activating monitor socket');
-    this.gembird.switch(this.plugMonitor, true);
-    console.log('activating speaker socket');
-    this.gembird.switch(this.plugSpeaker, true);
-    console.log('stopping squeezelite');
-    this.squeeze.stop();
+  if (0 === request.url.indexOf("/startWatching")) {
+    this.startWatching();
   }
 
-  if (0 === request.url.indexOf('/stopWatching')) {
-    console.log('Kodi screensaver activated');
-    this.gembird.switch(this.plugMonitor, false);
-    console.log('deactivating monitor socket');
-    this.squeeze.isPlaying(function (state) {
-      console.log('squeezelite is ' + (state ? 'playing' : 'not playing'));
-      if (!state) {
-        console.log('deactivating speaker socket');
-        this.gembird.switch(this.plugSpeaker, false);
-      }
-    }.bind(this));
+  if (0 === request.url.indexOf("/stopWatching")) {
+    this.stopWatching();
   }
   response.end();
+};
+
+Server.prototype.startMusic = function () {
+  console.log("Start Music");
+  console.log("activating speaker socket");
+  this.gembird.switch(this.plugSpeaker, true);
+  console.log("deactivating monitor socket");
+  this.gembird.switch(this.plugMonitor, false);
+  console.log("stopping active kodi players");
+  this.kodi.stopActivePlayers();
+};
+
+Server.prototype.stopMusic = function () {
+  console.log("Stop Music");
+  this.kodi.isActive(
+    function (active) {
+      console.log("Kodi screensaver  is " + (active ? "not active" : "active"));
+      if (!active) {
+        console.log("deactivating speaker socket");
+        this.gembird.switch(this.plugSpeaker, false);
+      }
+    }.bind(this)
+  );
+};
+
+Server.prototype.startWatching = function () {
+  console.log("Kodi screensaver deactivated");
+  console.log("activating monitor socket");
+  this.gembird.switch(this.plugMonitor, true);
+  console.log("activating speaker socket");
+  this.gembird.switch(this.plugSpeaker, true);
+  console.log("stopping squeezelite");
+  this.squeeze.stop();
+};
+
+Server.prototype.stopWatching = function () {
+  console.log("Kodi screensaver activated");
+  this.gembird.switch(this.plugMonitor, false);
+  console.log("deactivating monitor socket");
+  this.squeeze.isPlaying((playing) => {
+    console.log("squeezelite is " + (playing ? "playing" : "not playing"));
+    if (!playing) {
+      console.log("deactivating speaker socket");
+      this.gembird.switch(this.plugSpeaker, false);
+    }
+  });
 };
 
 server = new Server();
